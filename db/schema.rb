1e35982e0a6173fc396b6f2c93980a3d7df8c038
Olivia Grundman <oliviagrundman@gmail.com>
2022-08-30 21:07:02 +0100

    added full address to flats in schema & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_120013) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_200541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_120013) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "street_address"
+    t.string "city"
+    t.string "postcode"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
